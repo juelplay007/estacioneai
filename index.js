45fdd66e952dc4683648c6577d8a85cd09a3edd4
natan <natannatan123na@gmail.com>
2025-05-21 20:38:08 -0300

    mudanças no css e no htnl

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*let Estacionamento = Text 
 prompt ("Qual lugar você quer mapear")*/
 function cadastro() {
-    
+    b1 = document.getElementById("but")
+    b1.innerHTML = "Validação efetuada com sucesso" 
 }
