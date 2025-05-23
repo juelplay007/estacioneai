2f3b3a01ddeefaef09371a7b0df35b1339e9369d
juelplay007 <168240221+juelplay007@users.noreply.github.com>
2025-05-23 10:46:36 -0300

    alteraçao salva 1

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function cadastro() {
+function CadastroEstacionamento() {
     var b1 = document.getElementById("botCadastro");
-    b1.innerHTML = "Cadastro efetuado com sucesso";
+    b1.innerHTML = "Cadastro efetuado com sucesso!";
 }
 
 
-
-
-
-function procura() {
+function ProcuraEstacionamento() {
     var b2 = document.getElementById("botCadastro");
     b2.innerHTML = "Procurando...";
+}
+
+
+function Login() {
+    var b3 = document.getElementById("botCadastro");
+    b3.innerHTML = "Informe seu email e senha";
+}
+
+function CadastroUsuario() {
+var b4 = document.getElementById ("botCadastro");
+b4.innerHTML = "Digite seu nome,data de nascimento,endereço,email,senha";
+
 }
